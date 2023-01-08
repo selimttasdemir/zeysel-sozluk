c39730bd8098acc2f2f1af02e6cdffea2aec219e
selimttasdemir <selimtasdemir.2022@gmail.com>
2023-01-08 14:34:16 +0300

    GeneralSetup

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,11 +8,11 @@
     <title>@yield(title)</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    @yield(head)
+    @yield('head')
 </head>
 
 <body>
-    @yield(body_upper)
+    @yield('body_upper')
 
     <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
         <div class="dropdown">
@@ -50,9 +50,9 @@
         </div>
       </div>
     
-    @yield(content)
+    @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    @yield(body_lower)
+    @yield('body_lower')
 </body>
 
 </html>
